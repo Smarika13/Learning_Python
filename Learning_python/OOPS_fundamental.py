3aaf19cf2b2487139369a66435efb3ade6443704
Smarika <smarikakc96@gmail.com>
2026-05-05 21:07:43 +0545

    @property & @balance.setter

--- Learning_python/OOPS_fundamental.py
+++ Learning_python/OOPS_fundamental.py
@@ -1,30 +1,44 @@
-#OOPS fundamentals
+#OOPS fundamentals(inheritance,encapsulation)
 
 class BankAccount:
     def __init__ (self,owner,balance):
         self.owner = owner
-        self.balance = balance
+        self.__balance = balance
+
+    @property
+    def balance(self):
+        return self.__balance
+    
+    @balance.setter
+    def balance(self,amount):
+        if amount < 0:
+            print("Balance cannot be negative")
+        else:
+            self__balance = amount
 
     def deposit(self,amount):
-        self.balance += amount
-        print("The amount is",self.balance)
+        self.__balance += amount
+        print("The amount is",self.__balance)
 
     def withdraw(self, amount):
-        if amount > self.balance:
+        if amount > self.__balance:
             print("Insufficient funds")
         else:
-            self.balance -= amount
-            print("The amount is:",self.balance)
+            self.__balance -= amount
+            print("The amount is:",self.__balance)
 
     def get_balance(self):
-        return self.balance
+        return self.__balance
     
     def __str__(self):
-        return f"Account Owner: {self.owner} | Balance: {self.balance}"
+        return f"Account Owner: {self.owner} | Balance: {self.__balance}"
     
 
 account =BankAccount("Smarika", 1000)
 print(account)
+print(account.balance)
+account.balance = 500
+account.balance = -100
 account.deposit(500)
 account.withdraw(200)
 account.withdraw(2000)
@@ -36,15 +50,17 @@ print(account.get_balance())
 class SavingsAccount(BankAccount):
     def __init__(self,owner,balance, interest_rate):
         super().__init__(owner,balance)
-        self.interest_rate = interest_rate
+        self.__interest_rate = interest_rate
 
     def add_interest(self):
-        self.balance += self.balance * self.interest_rate
+        interest = self.get_balance() * self.__interest_rate
+        self.deposit(interest)
 
 acc =SavingsAccount("Smarika", 1000, 0.1)
 acc.deposit(500)
 acc.add_interest()
 print(acc.get_balance())
 print(acc)
+
 
 

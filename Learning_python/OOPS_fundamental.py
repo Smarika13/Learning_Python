940f7e95f12724a08da7b5d48712465760bd352a
Smarika <smarikakc96@gmail.com>
2026-05-05 20:32:22 +0545

    use of __Str__ method

--- Learning_python/OOPS_fundamental.py
+++ Learning_python/OOPS_fundamental.py
@@ -19,12 +19,17 @@ class BankAccount:
     def get_balance(self):
         return self.balance
     
+    def __str__(self):
+        return f"Account Owner: {self.owner} | Balance: {self.balance}"
+    
 
 account =BankAccount("Smarika", 1000)
+print(account)
 account.deposit(500)
 account.withdraw(200)
 account.withdraw(2000)
 print(account.get_balance())
+
 
 
 #Inheritance in OOPS
@@ -40,5 +45,6 @@ acc =SavingsAccount("Smarika", 1000, 0.1)
 acc.deposit(500)
 acc.add_interest()
 print(acc.get_balance())
+print(acc)
 
 

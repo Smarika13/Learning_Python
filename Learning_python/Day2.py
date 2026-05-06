9196e00ebd68adb328ee777eff48ee9d84523054
Smarika <smarikakc96@gmail.com>
2026-05-06 08:24:37 +0545

    Polymorphism basics

---
+++ Learning_python/Day2.py
@@ -0,0 +1,39 @@
+class Animal:
+    def speak(self):
+        return "Some sound"
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow"
+
+animals = [Dog(), Cat(), Dog()]
+
+for animal in animals:
+    print(animal.speak())
+
+#understanding basic of polymorphism 
+
+class Shape:
+    def area(self):
+        return 0
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+    def area(self):
+        return self.width * self.height
+
+shapes = [Circle(5), Rectangle(4, 6), Circle(3)]
+for shape in shapes:
+    print(shape.area())

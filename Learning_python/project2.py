9989430b867afecfd14c0e67df0fc485f8ba21bc
Smarika <smarikakc96@gmail.com>
2026-05-06 14:09:42 +0545

    Contact Book CLI using python

---
+++ Learning_python/project2.py
@@ -0,0 +1,76 @@
+#Contact Book CLI
+
+def add_contact(name,phone):
+    with open("contacts.txt", "a") as f:
+        f.write(f"{name} | {phone}\n")
+
+def view_contacts():
+    try:
+        with open("contacts.txt", "r") as f:
+            for line in f:
+                print(line.strip())
+    except FileNotFoundError:
+        print("File not found")
+
+def search_contact(name):
+    try:
+        with open("contacts.txt", "r") as f:
+            found = False
+            for line in f:
+                if name.lower() in line.lower():
+                    print(line.strip())
+                    found = True
+
+            if not found:
+                print(f"Not found contacts with name: {name}")
+    except FileNotFoundError:
+        print("File not found")
+
+def delete_contact(name):
+    try:
+        with open("contacts.txt", "r") as f:
+            lines = f.readlines()
+        found = False
+        new_lines = []
+        for line in lines:
+            if name in line:
+                found = True
+            else:
+                new_lines.append(line)
+
+        with open("contacts.txt", "w") as f:
+            f.writelines(new_lines)
+        if found:
+            print("Contact deleted")
+        else:
+            print("Contact not found")
+
+    except:
+        print("No contacts yet")
+
+
+while (True):
+    a = input("Enter any number from 1 to 5:")
+
+    if a == "1":
+        name = input("Enter the name you want to add:")
+        phone = int(input("Enter the number you want to add:"))
+        add_contact(name,phone)
+    elif a == "2":
+        view_contacts()
+    elif a == "3":
+        name = input("Enter the name you want to search:")
+        search_contact(name)
+    elif a == "4":
+        name = input("Enter the name you want to delete:")
+        delete_contact(name)
+    elif a == "5":
+        print("Exit")
+        break
+    else:
+        print("Invalid input.Enter from 1 to 5 only")
+
+
+
+
+
